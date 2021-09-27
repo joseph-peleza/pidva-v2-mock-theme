c03e58f84237152a3669e632b2c19fb5ae80ab1a
joseph-peleza <joseph.mbuku@peleza.com>
2021-09-27 15:08:20 +0300

    quick-fix: Customize Auth Page, Sign Up and Sign In with Peleza Logo and Git Ignore Netlify Config

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -50,7 +50,7 @@ class SignIn extends React.Component {
           <div className="gx-app-login-main-content">
             <div className="gx-app-logo-content">
               <div className="gx-app-logo-content-bg">
-                <img src="https://via.placeholder.com/272x395" alt='Neature'/>
+                <img src={require("assets/images/AuthLogo-272x395.png")} alt='Neature'/>
               </div>
               <div className="gx-app-logo-wid">
                 <h1><IntlMessages id="app.userAuth.signIn"/></h1>
